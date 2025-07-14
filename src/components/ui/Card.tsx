760cd917d1d06a6af7a41f5e936b72ec49a4925c
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-07-15 01:50:20 +0530

    fixed the Card component

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,24 +1,35 @@
-import { HTMLAttributes, forwardRef } from 'react'
+import { type HTMLAttributes, forwardRef } from 'react'
 import { motion } from 'framer-motion'
 
 interface CardProps extends HTMLAttributes<HTMLDivElement> {
-  hover?: boolean
+	hover?: boolean
 }
 
 export const Card = forwardRef<HTMLDivElement, CardProps>(
-  ({ hover = true, className = '', children, ...props }, ref) => {
-    const Component = hover ? motion.div : 'div'
-    
-    return (
-      <Component
-        ref={ref}
-        className={`card ${className}`}
-        whileHover={hover ? { y: -5, scale: 1.02 } : undefined}
-        transition={{ type: 'spring', stiffness: 300 }}
-        {...props}
-      >
-        {children}
-      </Component>
-    )
-  }
+	({ hover = true, className = '', children, ...props }, ref) => {
+		if (hover) {
+			return (
+				//@ts-ignore
+				<motion.div
+					ref={ref}
+					className={`card ${className}`}
+					whileHover={{ y: -5, scale: 1.02 }}
+					transition={{ type: 'spring', stiffness: 300 }}
+					{...props}
+				>
+					{children}
+				</motion.div>
+			)
+		}
+
+		return (
+			<div
+				ref={ref}
+				className={`card ${className}`}
+				{...props}
+			>
+				{children}
+			</div>
+		)
+	}
 )
